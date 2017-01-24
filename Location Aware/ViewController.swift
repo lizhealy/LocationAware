935de7ae2402d3edc5730349711699bbc4feca65
lizhealy <lizhealy8@gmail.com>
2017-01-24 12:44:28 -0500

    Initial Commit

--- Location Aware/ViewController.swift
+++ Location Aware/ViewController.swift
@@ -7,13 +7,93 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!
+    @IBOutlet weak var courseLabel: UILabel!
+    @IBOutlet weak var speedLabel: UILabel!
+    @IBOutlet weak var altitudeLabel: UILabel!
+    @IBOutlet weak var nearestAddressLabel: UILabel!
+    @IBOutlet weak var map: MKMapView!
+    
+    var locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
     }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        var userLocation: CLLocation = locations[0]
+        let latitude = userLocation.coordinate.latitude
+        let longitude = userLocation.coordinate.longitude
+        let latDelta: CLLocationDegrees = 0.05
+        let lonDelta: CLLocationDegrees = 0.05
+        let coordinates = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        let span: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lonDelta)
+        let region: MKCoordinateRegion = MKCoordinateRegion(center: coordinates, span: span)
+        
+        self.map.setRegion(region, animated: true)
+        
+        self.latitudeLabel.text = String(latitude)
+        self.longitudeLabel.text = String(longitude)
+        
+        self.courseLabel.text = String(userLocation.course)
+        self.altitudeLabel.text = String(userLocation.altitude)
+        self.speedLabel.text = String(userLocation.speed)
+        
+        CLGeocoder().reverseGeocodeLocation(userLocation) { (placemarks, error) in
+            if error != nil {
+                print(error!)
+            }
+            else {
+                if let placemark = placemarks?[0] {
+                    var subThoroughfare = ""
+                    if placemark.subThoroughfare != nil {
+                        subThoroughfare = placemark.subThoroughfare!
+                    }
+                    var thoroughfare = ""
+                    if placemark.thoroughfare != nil {
+                        thoroughfare = placemark.thoroughfare!
+                    }
+                    var subLocality = ""
+                    if placemark.subLocality != nil {
+                        subLocality = placemark.subLocality!
+                    }
+                    var subAdministrativeArea = ""
+                    if placemark.subAdministrativeArea != nil {
+                        subAdministrativeArea = placemark.subAdministrativeArea!
+                    }
+                    var administrativeArea = ""
+                    if placemark.administrativeArea != nil {
+                        administrativeArea = placemark.administrativeArea!
+                    }
+                    var postalCode = ""
+                    if placemark.postalCode != nil {
+                        postalCode = placemark.postalCode!
+                    }
+                    var country = ""
+                    if placemark.country != nil {
+                        country = placemark.country!
+                    }
+                    
+                    print(userLocation.altitude)
+                    print(subThoroughfare + " " + thoroughfare + "\n" + subLocality + "\n" + subAdministrativeArea + ", " + administrativeArea + "\n" + postalCode + "\n" + country)
+                    self.nearestAddressLabel.text = subThoroughfare + " " + thoroughfare + "\n" + subLocality + "\n" + subAdministrativeArea + ", " + administrativeArea + "\n" + postalCode + "\n" + country
+                }
+            }
+        }
+        
+        
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
